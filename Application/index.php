c2830c42348280e008fc9bad0524edea9d3c0e3b
Fl0rianL <Flo@MBP-Flo.local>
2014-06-17 09:13:21 +0200

    Ajout Application

---
+++ Application/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+<!-- Bootstrap css -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.theme.min.css">
+<link href="css/sb-admin.css" rel="stylesheet">
+</head>
+<body>
+
+<?php include('header.inc.php'); ?>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
+
+
+
+
+</body>
+</html>
